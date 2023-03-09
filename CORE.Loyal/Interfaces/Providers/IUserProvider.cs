b3d8720ba8ad33efcb648b29cde904c76dac3d4a
deyner2002 <solano27deiner@gmail.com>
2023-03-09 07:03:18 -0500

    Commit prueba

--- CORE.Loyal/Interfaces/Providers/IUserProvider.cs
+++ CORE.Loyal/Interfaces/Providers/IUserProvider.cs
@@ -1,9 +1,4 @@
 ﻿using Core.Loyal.Models.FTMUSIC;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CORE.Loyal.Interfaces.Providers
 {
